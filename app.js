f90e45d7b963db1d9ec5de2af854675157ccc59b
valentinoCogo <valentinocogo91@gmial.com>
2022-08-23 23:10:34 -0300

    comienzos del nav en index.html

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
+//
+
 const express = require("express");
 const app = express();
 const path = require("path");
-app.use(express.static(path.join(__dirname, './public')));
 
-app.listen(3050)
+//
 
-app.get("/",(req, res)=> {
-    res.sendFile(path.join(__dirname, "/views/login.html"))
-})
+app.use(express.static(path.join(__dirname, "./public")));
+
+app.listen(3050);
+
+app.get("/home", (req, res) => {
+  res.sendFile(path.join(__dirname, "/views/index.html"));
+});
+
+//
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "/views/login.html"));
+});
