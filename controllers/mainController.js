a1ca2d576688ef5e62214dd859bf2dffdf444a9a
valentinoCogo <valentinocogo91@gmial.com>
2023-01-18 19:22:51 -0300

    a

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -9,11 +9,18 @@ const controller = {
       css: "/home.css",
     });
   },
+  homeDos: function (req, res) {
+    res.render("homeDos", {
+      title: "Ghemma Store - Tienda oficial - Home",
+      css: "/homeDos.css",
+    });
+  },
   cart: function (req, res) {
-    res.render('cart', {title: "Ghemma Store - Tienda oficial",
-    css: "/cart.css",
-  });
-  }
+    res.render("cart", {
+      title: "Ghemma Store - Tienda oficial",
+      css: "/cart.css",
+    });
+  },
 
   //
 };
